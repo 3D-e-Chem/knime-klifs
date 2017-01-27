de82de62b4914a09f5f663422cefee69d1267651
AJK-dev <kooistra.a.j@gmail.com>
2017-01-27 13:58:24 +0100

    Updated for OSX-based testflows

--- tests/src/java/nl/vu_compmedchem/klifs/interactions/InteractionsGetTypesNodeModelTest.java
+++ tests/src/java/nl/vu_compmedchem/klifs/interactions/InteractionsGetTypesNodeModelTest.java
@@ -3,6 +3,8 @@ package nl.vu_compmedchem.klifs.interactions;
 import java.io.File;
 import java.io.IOException;
 
+import javax.swing.SwingUtilities;
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -29,7 +31,19 @@ public class InteractionsGetTypesNodeModelTest {
 	@Test
 	public void test_simple() throws IOException, InvalidSettingsException, CanceledExecutionException,
 	        UnsupportedWorkflowVersionException, LockFailedException, InterruptedException {
-	    File workflowDir = new File("src/knime/KLIFS-testflows");
-	    runner.runTestWorkflow(workflowDir);
+	    new Thread(new Runnable() {
+	    	public void run() {
+		    	SwingUtilities.invokeLater(new Runnable() {
+	      			public void run() {
+	      				File workflowDir = new File("src/knime/KLIFS-testflows");
+	      				try {
+			    			runner.runTestWorkflow(workflowDir);
+			    		} catch (Exception e){
+			    			System.out.println(e.toString());
+			    		}
+			    	}
+			    });
+		    }
+	    }).start();
 	}
 }
