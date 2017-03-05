cd31317028e743cbcf35e1e21ec6d1a408d67980
AJK-dev <kooistra.a.j@gmail.com>
2017-03-05 20:24:41 +0100

    Updated testflow for v1.1.1

--- nl.vu_compmedchem.klifs/src/nl/vu_compmedchem/klifs/structures/StructuresGetMOL2NodeModel.java
+++ nl.vu_compmedchem.klifs/src/nl/vu_compmedchem/klifs/structures/StructuresGetMOL2NodeModel.java
@@ -16,7 +16,6 @@ import org.knime.core.data.DataTableSpec;
 import org.knime.core.data.RowKey;
 import org.knime.core.data.def.DefaultRow;
 import org.knime.core.data.def.IntCell;
-import org.knime.core.data.def.StringCell;
 import org.knime.core.node.BufferedDataContainer;
 import org.knime.core.node.BufferedDataTable;
 import org.knime.core.node.CanceledExecutionException;
